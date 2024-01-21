d59e7f9672272764ec622182a358666e5ef2a288
MayankGaur1301 <gaurm924@gmail.com>
2024-01-21 15:15:04 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+const PORT = process.env.PORT || 8000;
 
 dotenv.config({
   path: "./env",
 });
 
-const app = express();
-connectDB();
+connectDB()
+  .then(() => {
+    //setting up an event listener for the "error" event, and when an error event occurs, the callback function is executed.
+    app.on("error", (error) => {
+      console.error("Error: ", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`server is running on port: http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed", err);
+  });
 
-/* one aproach is to connect database from index.js file ...but here we polluted the index.js file 
+/*=========================================================================================================
+ 1st => aproach is to connect database from index.js file ...but here we polluted the index.js file 
+2nd => approach is to create a separate file and write all connection code over there and then import here 
 (async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+/*This code defines a custom error class named ApiError in JavaScript. It extends the built-in Error class, providing additional properties and customization for handling errors in an API context */
+
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    statck = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (statck) {
+      this.stack = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
